b888f8fb277a3888f8e019c431e90568667ed3c4
viccy5022 <101460445+viccy5022@users.noreply.github.com>
2025-03-23 21:48:55 +0800

    Update get-password.js

--- api/get-password.js
+++ api/get-password.js
@@ -3,5 +3,10 @@ export default function handler(req, res) {
         return res.status(405).json({ error: "Method Not Allowed" });
     }
 
+    // 安全验证
+    if (req.query.secret !== process.env.UPDATE_SECRET) {
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+
     res.json({ password: process.env.API_PASSWORD });
 }
